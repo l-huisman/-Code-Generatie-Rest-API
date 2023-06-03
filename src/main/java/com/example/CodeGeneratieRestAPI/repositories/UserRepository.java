084243aaf46725de3aa731ae51cb25a73a401781
DevSoft <devonvanw@gmail.com>
2023-06-03 10:16:17 +0200

    Update UserRepository.java

--- src/main/java/com/example/CodeGeneratieRestAPI/repositories/UserRepository.java
+++ src/main/java/com/example/CodeGeneratieRestAPI/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.CodeGeneratieRestAPI.repositories;
 
 import com.example.CodeGeneratieRestAPI.models.User;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);    
     User findByUsernameAndPassword(String username, String password);
 }
 
