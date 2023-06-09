61cfba99d5af4aa96752f63c2c0ea5c955c72319
DevSoft <devonvanw@gmail.com>
2023-06-09 13:23:25 +0200

    Working on transactions

---
+++ src/main/java/com/example/CodeGeneratieRestAPI/exceptions/TransactionNotOwnedException.java
@@ -0,0 +1,11 @@
+package com.example.CodeGeneratieRestAPI.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "You do not own this transaction")
+public class TransactionNotOwnedException extends RuntimeException{
+    public TransactionNotOwnedException(String message) {
+        super(message);
+    }
+}
